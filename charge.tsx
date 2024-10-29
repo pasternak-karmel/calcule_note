126e8110eca092dae1747e8d4e1dea729535600b
pasternak-karmel <karmelavenon@gmail.com>
2024-10-29 16:23:30 +0100

    add shadcn component

---
+++ charge.tsx
@@ -0,0 +1,48 @@
+
+inscription connexion 
+gestion des rôles utilisateur (admin, proffesseur, étudiant)
+
+
+
+
+-admin 
+    -add professeur
+    -ajoute les étudiants
+    -il gere le systeme de clase (ue, ec, note, credit)
+    -cree le bulletin (pdf) 
+
+-professeur
+    -ajoute les notes
+
+-etudiant
+    -consulte les note
+    -consulte bulletin
+
+
+
+timeline du projet
+
+-inscription et connexion pages
+    -
+-admin
+    -Information de connexion (etudiant professeur)
+    -gestion de la structure de lecole
+        -les classes (LMD)
+        -Ue
+        -eu
+        -credit
+    -Gestion des professeurs
+        -Ajout et suppression des professeurs
+        -Attribution des classes
+    -Gestion des étudiants
+        -Ajout et suppression des étudiants
+        -Gestion de la conduite
+    -Rapport
+        -creation de bulletin
+
+-professeur
+    -Ajoute de les notes de chaques etudiant
+    -consulter
+    
+-etudiant
+    -consulter son bulletin
